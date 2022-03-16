a2dc37a3193f274b5a59e8e9186eaf561395e40d
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-03-16 18:21:02 +0100

    import

--- src/debut.lean
+++ src/debut.lean
@@ -1,6 +1,15 @@
 import data.rat.order
+import number_theory.zsqrtd.basic
+import algebra.order.archimedean
 
 lemma debut (x : ℚ) : ∃ (y : ℤ), |x - y| ≤ 1/2 :=
 begin
   sorry
+end
+
+open zsqrtd
+
+example (n : ℤ) (x y : ℤ√(-2)) : conj (0 : ℤ√(-2)) = 0 :=
+begin
+  simp,
 end
