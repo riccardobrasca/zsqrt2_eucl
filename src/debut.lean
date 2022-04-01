c4834808bab6dee976cacd651b774b5ef497ad79
Youri HAMON <youri76000@gmail.com>
2022-04-01 23:09:25 +0200

    tentative for all x in Q there exists y in Z such that abs(x-y) <= 1/2

--- src/debut.lean
+++ src/debut.lean
@@ -5,5 +5,14 @@ import algebra.order.archimedean
 
 lemma debut (x : ℚ) : ∃ (y : ℤ), |x - y| ≤ 1/2 :=
 begin
-  sorry
+  sorry,/- 
+  use round x,
+  rw round,
+  rw abs_sub_le_iff,
+  have h := int.floor_le (x + 1/2),
+  split,
+  {
+    rw sub_le,
+    rw le_trans ((x - 1 / 2) ≤ (↑⌊x + 1 / 2⌋)) h,
+  } -/
 end
